34e5d23a61f28a337e2bd3a554f1a096cc11c1ae
ajdickinson <adickin3@uoregon.edu>
2024-04-22 08:09:40 -0700

    Fixed K06b tests

--- tests/testthat/test-K06b_lm.R
+++ tests/testthat/test-K06b_lm.R
@@ -2,8 +2,9 @@ K06b <- read_file("../../R/K06b_lm.R")
 
 test_that("K06b: Question 1", {
   skip_incomplete(K06b, "01")
-  r <- parse_eval(str_match_q(K06b, "01"))
-  expect_true(grepl("qplot\\(data = gapminder, x = gdpPercap, y = lifeExp", deparse(r)))
+  r <- parse_eval(str_match_q(K06b, "01"))  # This should be checked if it actually returns a ggplot object.
+
+  expect_true(inherits(r, "gg")) # Check if 'r' is a ggplot object
 })
 
 test_that("K06b: Question 2", {
@@ -16,15 +17,15 @@ test_that("K06b: Question 2", {
 test_that("K06b: Question 3", {
   skip_incomplete(K06b, "03")
   r <- parse_eval(str_match_q(K06b, "03"))
-  r <- r[[1]] # Assuming the result is the first element in a list
-  expect_equal(names(r[[2, 1]]), "gdpPercap")
-  expect_true(r[[2, 5]] < 0.01) # Checking if p-value < 0.01
+  print(str(r))
+  # expect_equal(r$term[2], "gdpPercap")
+  expect_true(r$p.value[2] < 0.01)
 })
 
 test_that("K06b: Question 4", {
   skip_incomplete(K06b, "04")
   r <- parse_eval(str_match_q(K06b, "04"))
-  expect_equal(names(r), "r.squared")
+  r$r.squared %>% between(0.34, 0.35) %>% expect_true()
 })
 
 test_that("K06b: Question 5", {
@@ -42,7 +43,7 @@ test_that("K06b: Question 6", {
 test_that("K06b: Question 7", {
   skip_incomplete(K06b, "07")
   r <- parse_eval(str_match_q(K06b, "07"))
-  expect_true(is.numeric(r)) # Check if 'rss' is a numeric value
+  expect_true(between(r, 187335, 187336))
 })
 
 test_that("K06b: Question 8", {
@@ -54,12 +55,16 @@ test_that("K06b: Question 8", {
 test_that("K06b: Question 9", {
   skip_incomplete(K06b, "09")
   r <- parse_eval(str_match_q(K06b, "09"))
-  expect_true(grepl("qplot\\(data = gapminder, x = gdpPercap, y = lifeExp", deparse(r$call))) &&
-    grepl("geom_smooth\\(method = \"lm\"", deparse(r$call))
+  is_geom_smooth <- r$layers[[2]]$geom |> class() |> str_detect("GeomSmooth")
+
+  # Now accessing the first element
+  result <- is_geom_smooth[[1]]
+
+  expect_true(result)
 })
 
 test_that("K06b: Question 10", {
   skip_incomplete(K06b, "10")
   r <- parse_eval(str_match_q(K06b, "10"))
-  expect_equal(r[[2, 1]], "pop")
+  expect_true(r$term[3] == "pop")
 })
